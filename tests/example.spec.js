edd59fb748aba905ae0651cbb0a34d7a16cf9b7d
Kailash Chander Pathak <you@example.com>
2024-05-26 20:06:12 +0530

    disable the flag

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -9,13 +9,10 @@ test('TF #01,TS #01 has title', async ({ page }) => {
 });
 
 test('TF #01,TS #02 get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://www.lambdatest.com/blog');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Expect a title "to contain" a substring.
+ await expect(page).toHaveTitle(/LambdaTest Blogs/);
 });
 
 
@@ -27,11 +24,8 @@ test('TF #01,TS #03 has title', async ({ page }) => {
 });
 
 test('TF #01,TS #04 get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://www.lambdatest.com/blog');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Expect a title "to contain" a substring.
+ await expect(page).toHaveTitle(/LambdaTest Blogs/);
 });
